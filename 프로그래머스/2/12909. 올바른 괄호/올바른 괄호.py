b1cca65a7bf8411d3a8c15a3f53ab99732b561cf
rOyalFruit <96731051+rOyalFruit@users.noreply.github.com>
2025-05-13 21:21:27 +0900

    [level 2] Title: 올바른 괄호, Time: 12.19 ms, Memory: 9.54 MB -BaekjoonHub

--- 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.py
+++ 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.py
@@ -1,10 +1,13 @@
 def solution(s):
     lst = []
+    
     for char in s:
+        if len(lst) <= 0 and char == ')':
+            return False
+            
         if char == '(':
             lst.append(char)
         else:
-            if len(lst) <= 0:
-                return False
             lst.pop(-1)
+            
     return len(lst) == 0
