f8e97ce3dceecb7bd4048b34dc9e6e92643e499d
rOyalFruit <96731051+rOyalFruit@users.noreply.github.com>
2025-05-22 19:40:05 +0900

    [level 2] Title: 카펫, Time: 0.10 ms, Memory: 9.26 MB -BaekjoonHub

--- 프로그래머스/2/42842. 카펫/카펫.py
+++ 프로그래머스/2/42842. 카펫/카펫.py
@@ -1,15 +1,5 @@
-def find_divisor_pairs(yellow):
-    result = []
+def solution(brown, yellow):
     for i in range(1, int(yellow**0.5)+1):
         if yellow % i == 0:
-            result.append((yellow//i, i))
-    return result
-
-def solution(brown, yellow):
-    answer = []
-    divisor_pairs = find_divisor_pairs(yellow)
-    for width, height in divisor_pairs:
-        if (width+2)*2 + height*2 == brown:
-            return [width+2, height+2]
-    return answer
-
+            if (yellow//i+2)*2 + i*2 == brown:
+                return [yellow//i+2, i+2]
