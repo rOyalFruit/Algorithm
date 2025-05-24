945ed7fbcefdb342ea6c65f1b0f418c11063bf77
rOyalFruit <96731051+rOyalFruit@users.noreply.github.com>
2025-05-24 09:25:57 +0900

    [level 2] Title: 피로도, Time: 40.10 ms, Memory: 9.09 MB -BaekjoonHub

--- 프로그래머스/2/87946. 피로도/피로도.py
+++ 프로그래머스/2/87946. 피로도/피로도.py
@@ -1,20 +1,3 @@
-from itertools import permutations
-        
-
 def solution(k, dungeons):
-    answer = 0
-    
-    for perm in permutations(dungeons):
-        count = 0
-        fatigue = k
-    
-        for required_k, cost in perm:
-            if required_k <= fatigue:
-                count += 1
-                fatigue -= cost
-            else:
-                break
-        
-        answer = max(answer, count)
-        
+    answer = -1
     return answer
