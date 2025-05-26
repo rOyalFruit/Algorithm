0426321423c1dab5ed9898eba4aba2e6d5bd3a56
rOyalFruit <96731051+rOyalFruit@users.noreply.github.com>
2025-05-26 22:34:43 +0900

    [level 2] Title: 피로도, Time: 40.10 ms, Memory: 9.09 MB -BaekjoonHub

--- 프로그래머스/2/87946. 피로도/피로도.py
+++ 프로그래머스/2/87946. 피로도/피로도.py
@@ -1,31 +1,10 @@
-def find_permutations(current, remaining):
-    result = []
-    
-    if not remaining:  # 모든 요소가 선택되었을 때 (완전한 순열)
-        if current:  # 빈 리스트가 아닌 경우에만 추가
-            result.append(current)
-        return result
-    
-    # 길이가 1부터 최대 길이의 순열까지 모든 결과물을 포함하려면 리턴 조건 수정하면 됨.
-    # 
-    # if current: # 현재 순열을 결과에 추가 (빈 리스트가 아닌 경우)
-    #     result.append(current)
-    #
-    # if not remaining: # 남은 요소가 없으면 현재 결과 반환
-    #     return result
-    
-    for i in range(len(remaining)):
-        new_current = current + [remaining[i]]
-        new_remaining = remaining[:i] + remaining[i+1:]
-        result.extend(find_permutations(new_current, new_remaining))
-        
-    return result
+from itertools import permutations
         
 
 def solution(k, dungeons):
     answer = 0
     
-    for perm in find_permutations([], dungeons):
+    for perm in permutations(dungeons):
         count = 0
         fatigue = k
     
