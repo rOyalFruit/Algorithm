7e7ccf769b320041cc056ee29cee25a532397685
rOyalFruit <96731051+rOyalFruit@users.noreply.github.com>
2025-06-10 23:42:45 +0900

    [level 1] Title: 가장 많이 받은 선물, Time: 5.41 ms, Memory: 10 MB -BaekjoonHub

--- 프로그래머스/1/258712. 가장 많이 받은 선물/가장 많이 받은 선물.py
+++ 프로그래머스/1/258712. 가장 많이 받은 선물/가장 많이 받은 선물.py
@@ -1,39 +1,38 @@
+from itertools import combinations
+
+
 def solution(friends, gifts):
+    friend_index = {name: idx for idx, name in enumerate(friends)}
     n = len(friends)
-    friends_dict = {k:v for v, k in enumerate(friends)}
-    gift_graph = [[0] * n for _ in range(n)]
-    gift_score = [0] * n
     
-    # 주고받은 선물
+    # 1. 선물 교환 기록 행렬 생성
+    gift_matrix = [[0] * n for _ in range(n)]
     for gift in gifts:
-        sender, reciever = gift.split(" ")
-        gift_graph[friends_dict[sender]][friends_dict[reciever]] += 1
-    
-    # 선물 지수 구하기
-    for i in range(n):
-        gift_score[i] = sum(gift_graph[i]) - sum(recieved[i] for recieved in gift_graph)
+        giver, receiver = gift.split()
+        giver_idx = friend_index[giver]
+        receiver_idx = friend_index[receiver]
+        gift_matrix[giver_idx][receiver_idx] += 1
         
-    # 다음 달에 받을 선물 수 구하기
-    temp = [0] * n
-    visited = [[False] * n for _ in range(n)]
-    for i in range(0, n):
-        for j in range(0, n):
-            if i == j:
-                continue
-                
-            if not visited[i][j] and not visited[j][i]:
-                if gift_graph[i][j] > gift_graph[j][i]:
-                    temp[i] += 1
-                elif gift_graph[i][j] < gift_graph[j][i]:
-                    temp[j] += 1
-                else:
-                    if gift_score[i] > gift_score[j]:
-                        temp[i] += 1
-                    elif gift_score[i] < gift_score[j]:
-                        temp[j] += 1
-                
-                visited[i][j] = True
-                visited[j][i] = True
-                    
-    # 가장 많은 선물을 받는 친구가 받을 선물의 수 구하기
-    return max(temp)
+    # 2. 선물 지수 계산 (주고받은 선물 차이)
+    gift_scores = [
+        sum(given) - sum(received[me] for received in gift_matrix) for me, given in enumerate(gift_matrix)
+    ]
+    
+    # 3. 다음 달 예상 선물 계산
+    next_month = [0] * n
+    
+    for a, b in combinations(range(n), 2):
+        a_to_b = gift_matrix[a][b]
+        b_to_a = gift_matrix[b][a]
+        
+        if a_to_b > b_to_a:
+            next_month[a] += 1
+        elif a_to_b < b_to_a:
+            next_month[b] += 1
+        else:
+            if gift_scores[a] > gift_scores[b]:
+                next_month[a] += 1
+            elif gift_scores[a] < gift_scores[b]:
+                next_month[b] += 1
+    
+    return max(next_month)
