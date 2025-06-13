79702db0d434263858cbd98ff468df7597e74032
rOyalFruit <96731051+rOyalFruit@users.noreply.github.com>
2025-06-13 18:20:48 +0900

    [level 2] Title: 거리두기 확인하기, Time: 0.06 ms, Memory: 9.11 MB -BaekjoonHub

--- 프로그래머스/2/81302. 거리두기 확인하기/README.md
+++ 프로그래머스/2/81302. 거리두기 확인하기/README.md
@@ -1,10 +1,10 @@
 # [level 2] 거리두기 확인하기 - 81302 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/81302) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/81302#fn1) 
 
 ### 성능 요약
 
-메모리: 9.33 MB, 시간: 0.06 ms
+메모리: 9.11 MB, 시간: 0.06 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 06월 13일 18:06:16
+2025년 06월 13일 18:20:11
 
 ### 문제 설명
 
--- 프로그래머스/2/81302. 거리두기 확인하기/거리두기 확인하기.py
+++ 프로그래머스/2/81302. 거리두기 확인하기/거리두기 확인하기.py
@@ -1,3 +1,32 @@
+from itertools import combinations
+
+
+def is_violated(p1, p2, place):
+    r1, c1 = p1
+    r2, c2 = p2
+    dist = abs(r1 - r2) + abs(c1 - c2) # 맨해튼 거리 계산
+    
+    if dist > 2:  # 거리가 2 초과면 거리두기 위반 아님
+        return False
+    if r1 == r2:  # 같은 행
+        return place[r1][(c1 + c2) // 2] in ['O', 'P']
+    if c1 == c2:  # 같은 열
+        return place[(r1 + r2) // 2][c1] in ['O', 'P']
+    return place[r1][c2] in ['O', 'P'] or place[r2][c1] in ['O', 'P']  # 대각선
+
+
 def solution(places):
     answer = []
+    for place in places:
+        # 모든 사람(P)의 위치를 리스트로 저장
+        people = [(i, j) for i in range(5) for j in range(5) if place[i][j] == 'P']
+        
+        # 모든 사람 쌍에 대해 거리두기 위반 여부 확인
+        for p1, p2 in combinations(people, 2):
+            if is_violated(p1, p2, place):
+                answer.append(0)
+                break
+        else:
+            answer.append(1)
+            
     return answer
