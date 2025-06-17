649f588d8029c57dc284d88d4d9f8ad9c47b6ead
rOyalFruit <96731051+rOyalFruit@users.noreply.github.com>
2025-06-17 15:33:37 +0900

    [level 3] Title: [카카오 인턴] 경주로 건설, Time: 9.12 ms, Memory: 9.21 MB -BaekjoonHub

---
+++ 프로그래머스/3/67259. ［카카오 인턴］ 경주로 건설/［카카오 인턴］ 경주로 건설.py
@@ -0,0 +1,26 @@
+import heapq
+
+
+def solution(board):
+    costs = [[[float('inf')] * 4 for _ in range(len(board))] for _ in range(len(board))]
+    costs[0][0] = [0] * 4
+    heap = [(0, 0, 0, -1)]  # 비용, i, j, 방향
+    
+    while heap:
+        cur_cost, ci, cj, prev_dir = heapq.heappop(heap)
+        
+        for cur_dir, (di, dj) in enumerate(zip([1, -1, 0, 0], [0, 0, 1, -1])):
+            ni = ci + di
+            nj = cj + dj
+            
+            if 0 <= ni < len(board) and 0 <= nj < len(board) and board[ni][nj] == 0:
+                next_cost = cur_cost + 100
+                
+                if prev_dir != -1 and prev_dir != cur_dir:
+                    next_cost += 500
+                
+                if next_cost < costs[ni][nj][cur_dir]:
+                    costs[ni][nj][cur_dir] = next_cost
+                    heapq.heappush(heap, (next_cost, ni, nj, cur_dir))
+                    
+    return min(costs[-1][-1])
