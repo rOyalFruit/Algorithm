fdeb3a00354e9e07c8e6052461ef3010e813ac63
rOyalFruit <96731051+rOyalFruit@users.noreply.github.com>
2025-07-06 18:50:29 +0900

    [level 2] Title: 튜플, Time: 32.75 ms, Memory: 22.6 MB -BaekjoonHub

--- 프로그래머스/2/64065. 튜플/튜플.py
+++ 프로그래머스/2/64065. 튜플/튜플.py
@@ -1,28 +1,12 @@
-def parse_string(s):
-    content = s[2:-2]             # "2},{2,1},{2,1,3},{2,1,3,4"
-    parts = content.split('},{')  # ['2', '2,1', '2,1,3', '2,1,3,4']
-    result = []
-    
-    for part in parts:
-        # '2,1' -> ['2', '1'] -> [2, 1]
-        numbers = [int(num) for num in part.split(',')]
-        result.append(numbers)
-        
-    return result
-
-
 def solution(s):
-    content = parse_string(s)
-    content.sort(key=len)
-    nums_set = set()
     answer = []
+    parts = s[2:-2].split('},{')
+    sets = sorted([set(map(int, p.split(','))) for p in parts], key=len)
+    previous_set = set() 
     
-    for lst in content:
-        for num in lst:
-            if num in nums_set:
-                continue
-                
-            nums_set.add(num)
-            answer.append(num)
-            
+    for current_set in sets:
+        new_element = list(current_set - previous_set)[0]
+        answer.append(new_element)
+        previous_set = current_set 
+        
     return answer
