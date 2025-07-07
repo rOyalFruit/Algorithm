25b8aed2a659d10a5963030b4d55b44b77d6375f
rOyalFruit <96731051+rOyalFruit@users.noreply.github.com>
2025-07-07 12:37:24 +0900

    [level 2] Title: 2 x n 타일링, Time: 5.18 ms, Memory: 9.18 MB -BaekjoonHub

--- 프로그래머스/2/12900. 2 x n 타일링/2 x n 타일링.py
+++ 프로그래머스/2/12900. 2 x n 타일링/2 x n 타일링.py
@@ -1,13 +1,10 @@
 def solution(n):
-    answer = 0
-    a, b = 1, 2
+    a, b = 1, 1
     
-    if n <= 2:
-        return n
-    
-    for i in range(3, n+1):
-        answer = (a + b) % 1_000_000_007
-        a = b
-        b = answer
+    for i in range(1, n):
+        a, b = b, (a+b) % 1_000_000_007
         
-    return answer
+    return b
+
+
+        
