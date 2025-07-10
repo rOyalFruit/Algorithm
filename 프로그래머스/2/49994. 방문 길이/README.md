aa5717299fca7da2f012d947a1ca85134306d717
rOyalFruit <96731051+rOyalFruit@users.noreply.github.com>
2025-07-10 17:55:09 +0900

    [level 2] Title: 방문 길이, Time: 0.39 ms, Memory: 9.2 MB -BaekjoonHub

--- 프로그래머스/2/49994. 방문 길이/README.md
+++ 프로그래머스/2/49994. 방문 길이/README.md
@@ -1,10 +1,10 @@
 # [level 2] 방문 길이 - 49994 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/49994) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/49994#) 
 
 ### 성능 요약
 
-메모리: 9.25 MB, 시간: 2.33 ms
+메모리: 9.2 MB, 시간: 0.39 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 07월 10일 17:53:01
+2025년 07월 10일 17:54:58
 
 ### 문제 설명
 
--- 프로그래머스/2/49994. 방문 길이/방문 길이.py
+++ 프로그래머스/2/49994. 방문 길이/방문 길이.py
@@ -1,18 +1,15 @@
 def solution(dirs):
-    answer = 0
     ci, cj = 0, 0
     d = {"U": (-1, 0), "D": (1, 0), "R": (0, 1), "L": (0, -1)}
-    path = []
+    path = set()
     
     for dir in dirs:
         di, dj = d[dir]
         ni, nj = ci + di, cj + dj
         
         if -5 <= ni <= 5 and -5 <= nj <= 5:
-            if (ci, cj, ni, nj) not in path and (ni, nj, ci, cj) not in path:
-                path.append((ci, cj, ni, nj))
-                path.append((ni, nj, ci, cj))
-                answer += 1
+            path.add((ci, cj, ni, nj))
+            path.add((ni, nj, ci, cj))
             ci, cj = ni, nj
             
-    return answer
+    return len(path) // 2
