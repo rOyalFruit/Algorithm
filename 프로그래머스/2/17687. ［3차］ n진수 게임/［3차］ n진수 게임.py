b31a34bf1030b5a88b427872a90d1179a31fbb79
rOyalFruit <96731051+rOyalFruit@users.noreply.github.com>
2025-07-11 16:27:06 +0900

    [level 2] Title: [3차] n진수 게임, Time: 21.31 ms, Memory: 9.15 MB -BaekjoonHub

--- 프로그래머스/2/17687. ［3차］ n진수 게임/［3차］ n진수 게임.py
+++ 프로그래머스/2/17687. ［3차］ n진수 게임/［3차］ n진수 게임.py
@@ -1,22 +1,26 @@
-from collections import deque
-
-
 def solution(n, t, m, p):
-    d = {10: "A", 11: "B", 12: "C", 13: "D", 14: "E", 15: "F"}
-    result = [0]
-    i = 1
-    
-    while True:
-        if len(result) >= t * m:
-            break
-            
-        dq = deque()
-        num = i
-        while num != 0:
-            num, remainder = divmod(num, n)
-            dq.appendleft(remainder)
+    digits = "0123456789ABCDEF"
 
-        result.extend([i if i < 10 else d[i] for i in dq])
-        i += 1
+    def convert_base(num, base):
+        """숫자를 base 진법 문자열로 변환하는 헬퍼 함수"""
+        if num == 0:
+            return "0"
+        
+        result = ""
+        while num > 0:
+            num, remainder = divmod(num, base)
+            result = digits[remainder] + result # 나머지에 해당하는 문자를 앞에 추가
+        return result
+
+    # 1. 게임에서 말하는 전체 숫자 문자열 생성
+    game_string = ""
+    num = 0
+    required_length = m * t 
     
-    return "".join(map(str, result[p-1:t*m:m]))
+    while len(game_string) < required_length:
+        game_string += convert_base(num, n)
+        num += 1
+
+    # 2. 튜브가 말해야 하는 숫자들만 추출
+    return game_string[p-1:t*m:m]
+
