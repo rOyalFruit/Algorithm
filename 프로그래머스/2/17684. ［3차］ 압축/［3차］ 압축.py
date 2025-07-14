e3507b6010b907bec7f2ed4e35d53a2746e1f8d0
rOyalFruit <96731051+rOyalFruit@users.noreply.github.com>
2025-07-14 18:35:53 +0900

    [level 2] Title: [3차] 압축, Time: 0.97 ms, Memory: 9.27 MB -BaekjoonHub

--- 프로그래머스/2/17684. ［3차］ 압축/［3차］ 압축.py
+++ 프로그래머스/2/17684. ［3차］ 압축/［3차］ 압축.py
@@ -1,25 +1,27 @@
-from collections import defaultdict
-
-
 def solution(msg):
+    # 1. 사전 초기화 (A-Z)
     answer = []
-    idx = 1
-    d = defaultdict(int)
-    for i in range(65, 90+1):
-        d[chr(i)] = idx
-        idx += 1
-    
-    word = ""
-    prev = msg[0]
-    for i in range(1, len(msg)):
-        word = prev + msg[i]
-        if word in d:
-            prev = word
-            continue
-        answer.append(d[prev])
-        prev = msg[i]
-        d[word] = idx
-        idx += 1
-    answer.append(d[prev])
-    
+    dictionary = {chr(i):i-64 for i in range(65, 90+1)}
+    next_idx = 27
+
+    i = 0
+    while i < len(msg):
+        # 2. 현재 위치에서 가장 긴 단어(w) 찾기
+        w = msg[i]
+        while i + len(w) < len(msg) and w + msg[i + len(w)] in dictionary:
+            w += msg[i + len(w)]
+
+        # 3. w에 해당하는 색인 번호 출력
+        answer.append(dictionary[w])
+
+        # 4. 입력에서 처리한 w 제거 및 사전에 w+c 등록
+        # 다음 글자(c)가 있는 경우에만 사전에 추가
+        if i + len(w) < len(msg):
+            c = msg[i + len(w)]
+            dictionary[w + c] = next_idx
+            next_idx += 1
+        
+        # 5. 현재 위치(i)를 w의 길이만큼 이동
+        i += len(w)
+        # print(f"{w=}, {c=}, {answer=}")
     return answer
