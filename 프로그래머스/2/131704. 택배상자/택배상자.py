a2eaa9b4f857d9540f870e4c43da921b11782d9f
rOyalFruit <96731051+rOyalFruit@users.noreply.github.com>
2025-07-15 17:08:56 +0900

    [level 2] Title: 택배상자, Time: 231.25 ms, Memory: 52.4 MB -BaekjoonHub

--- 프로그래머스/2/131704. 택배상자/택배상자.py
+++ 프로그래머스/2/131704. 택배상자/택배상자.py
@@ -1,22 +1,12 @@
-from collections import deque
-
-
 def solution(order):
-    order = deque(order)
     answer = 0
     stack = []
     
     for i in range(1, len(order)+1):
-        if i == order[0]:
-            order.popleft()
-            answer += 1
-            
-            while stack and stack[-1] == order[0]:
-                stack.pop()
-                order.popleft()
-                answer += 1
-                
-        else:
-            stack.append(i)
-        
+        stack.append(i)
+
+        while stack and stack[-1] == order[answer]:
+            stack.pop()
+            answer +=1
+
     return answer
