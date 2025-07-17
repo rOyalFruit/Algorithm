3dd912bdc14182c67eb1384e3c5714bea8cd4929
rOyalFruit <96731051+rOyalFruit@users.noreply.github.com>
2025-07-17 15:51:08 +0900

    [level 3] Title: 숫자 게임, Time: 47.39 ms, Memory: 17.5 MB -BaekjoonHub

--- 프로그래머스/3/12987. 숫자 게임/숫자 게임.py
+++ 프로그래머스/3/12987. 숫자 게임/숫자 게임.py
@@ -2,12 +2,11 @@ def solution(A, B):
     answer = 0
     A.sort()
     B.sort()
-    
     a_idx = 0
+    
     for i in range(len(B)):
-        if B[i] <= A[a_idx]:
-            continue
-        answer += 1
-        a_idx += 1
+        if A[a_idx] < B[i]:
+            answer += 1
+            a_idx += 1
             
     return answer
