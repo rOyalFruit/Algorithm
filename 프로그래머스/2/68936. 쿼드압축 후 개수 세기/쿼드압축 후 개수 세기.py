db7f4922098d0219928fecaebb99598997213593
rOyalFruit <96731051+rOyalFruit@users.noreply.github.com>
2025-07-24 18:28:08 +0900

    [level 2] Title: 쿼드압축 후 개수 세기, Time: 236.79 ms, Memory: 11.5 MB -BaekjoonHub

--- 프로그래머스/2/68936. 쿼드압축 후 개수 세기/쿼드압축 후 개수 세기.py
+++ 프로그래머스/2/68936. 쿼드압축 후 개수 세기/쿼드압축 후 개수 세기.py
@@ -1,3 +1,29 @@
 def solution(arr):
-    answer = []
+    def recursive(row, col, size):
+        nonlocal answer
+        start_val = arr[row][col]
+        
+        is_valid = True
+        for r in range(row, row+size):
+            for c in range(col, col+size):
+                if arr[r][c] != start_val:
+                    is_valid = False
+                    break
+            if not is_valid:
+                break
+        
+        if is_valid:
+            answer[start_val] += 1
+            return
+        
+        half = size // 2
+        recursive(row, col, half)
+        recursive(row, col+half, half)
+        recursive(row+half, col, half)
+        recursive(row+half, col+half, half)
+        
+    
+    answer = [0, 0]
+    recursive(0, 0, len(arr))
+    
     return answer
